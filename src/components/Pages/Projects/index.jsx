8ca667741ba49cb62898f5c54aaa06bf3f3eaa4a
Benihoud Khalil <khalil.benihoud@gmail.com>
2017-05-17 08:53:32 +0200

    Templating fixes

--- src/components/Pages/Projects/index.jsx
+++ src/components/Pages/Projects/index.jsx
@@ -20,20 +20,24 @@ class Projects extends Component {
       const details = projects[0]['details'];
       const project = details[this.state._id];
       return (
-          <section id="Projects" className="cover cf center ph3  bg-mid-gray ">
-            <Header />
-              <div className="projects-top db dt-ns mw9 center w-100 tc">
-                <div className="pb3 pb4-ns pt4 pt4-ns mt4 white-70">
-                  <time className="f5 f4-l db fw1 baskerville mb2"> March 19th, 2015</time>
-                  <h2 className="tc f1 lh-title mt0 i">
-                    {project.projectTitle}
-                  </h2>
+          <section id="">
+            <div id="Projects" className="cover cf center ph3">
+              <Header />
+                <div className="projects-top db dt-ns mw9 center w-100 tc">
+                  <div className="pb3 pb4-ns pt4 pt4-ns mt4 white-70">
+                    <time className="f5 f4-l db fw1 baskerville mb2"> March 19th, 2015</time>
+                    <h2 className="tc f1 lh-title mt0 i">
+                      {project.projectTitle}
+                    </h2>
+                  </div>
                 </div>
               </div>
-              <div className="projects-infos">
-                <h3>Overview</h3>
-                <p>{project.projectOverview}</p>
-              </div>
+                <div className="projects-infos db dt-ns mw9 center w-100 ">
+                  <h3>Overview</h3>
+                  <div className="center measure-wide f5 pv5 lh-copy ph2">
+                    <p>{project.projectOverview}</p>
+                  </div>
+                </div>
           </section>
 
 
